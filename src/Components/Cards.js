437a2119c98ba91dafea2add8fa202b30cb5cbdf
Lusine Papanyan <lpapanyan9611@gmail.com>
2024-04-19 15:45:27 +0400

    shuffle cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,24 +2,26 @@ import { useState } from "react";
 import Card from "./card";
 
 function Cards() {
-  const [cards, setCards] = useState([
-    { id: 0, name: "Dayaneris Targaryan", status: "", img: "/images/01.jpg" },
-    { id: 0, name: "Dayaneris Targaryan", status: "", img: "/images/01.jpg" },
-    { id: 1, name: "Tirion Lanister", status: "", img: "/images/02.jpg" },
-    { id: 1, name: "Tirion Lanister", status: "", img: "/images/02.jpg" },
-    { id: 2, name: "Jon Snow", status: "", img: "/images/03.jpg" },
-    { id: 2, name: "Jon Snow", status: "", img: "/images/03.jpg" },
-    { id: 3, name: "Joefrey Barateon", status: "", img: "/images/04.jpg" },
-    { id: 3, name: "Joefrey Barateon", status: "", img: "/images/04.jpg" },
-    { id: 4, name: "Cersei Lanister", status: "", img: "/images/05.jpg" },
-    { id: 4, name: "Cersei Lanister", status: "", img: "/images/05.jpg" },
-    { id: 5, name: "Eddart Stark", status: "", img: "/images/06.jpg" },
-    { id: 5, name: "Eddart Stark", status: "", img: "/images/06.jpg" },
-    { id: 6, name: "Tywin Lanister", status: "", img: "/images/07.jpg" },
-    { id: 6, name: "Tywin Lanister", status: "", img: "/images/07.jpg" },
-    { id: 7, name: "Sandor Clegane", status: "", img: "/images/08.jpg" },
-    { id: 7, name: "Sandor Clegane", status: "", img: "/images/08.jpg" },
-  ]);
+  const [cards, setCards] = useState(
+    [
+      { id: 0, name: "Dayaneris Targaryan", status: "", img: "/images/01.jpg" },
+      { id: 0, name: "Dayaneris Targaryan", status: "", img: "/images/01.jpg" },
+      { id: 1, name: "Tirion Lanister", status: "", img: "/images/02.jpg" },
+      { id: 1, name: "Tirion Lanister", status: "", img: "/images/02.jpg" },
+      { id: 2, name: "Jon Snow", status: "", img: "/images/03.png" },
+      { id: 2, name: "Jon Snow", status: "", img: "/images/03.png" },
+      { id: 3, name: "Joefrey Barateon", status: "", img: "/images/04.jpg" },
+      { id: 3, name: "Joefrey Barateon", status: "", img: "/images/04.jpg" },
+      { id: 4, name: "Cersei Lanister", status: "", img: "/images/05.jpg" },
+      { id: 4, name: "Cersei Lanister", status: "", img: "/images/05.jpg" },
+      { id: 5, name: "Eddart Stark", status: "", img: "/images/06.jpg" },
+      { id: 5, name: "Eddart Stark", status: "", img: "/images/06.jpg" },
+      { id: 6, name: "Tywin Lanister", status: "", img: "/images/07.jpg" },
+      { id: 6, name: "Tywin Lanister", status: "", img: "/images/07.jpg" },
+      { id: 7, name: "Sandor Clegane", status: "", img: "/images/08.jpg" },
+      { id: 7, name: "Sandor Clegane", status: "", img: "/images/08.jpg" },
+    ].sort(() => Math.random() - 0.5)
+  );
   return (
     <div className="container">
       {cards.map((card, index) => {
