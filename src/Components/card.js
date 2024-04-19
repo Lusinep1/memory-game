6f82e2ff90da54fa7210873237e5b21ee06ad3f5
Lusine Papanyan <lpapanyan9611@gmail.com>
2024-04-19 16:23:23 +0400

    working!!!

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,6 +1,6 @@
 export default function Card({ card, index, clickHandler }) {
   return (
-    <div onClick={() => clickHandler(index)} className="card">
+    <div onClick={() => clickHandler(index)} className={`card ${card.status}`}>
       <img src={card.img} alt={card.name} />
     </div>
   );
