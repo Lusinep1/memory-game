c0fd778730b510dad80b712e69a9f5f9ae641f8b
Lusine Papanyan <lpapanyan9611@gmail.com>
2024-04-19 15:39:36 +0400

    add styles and basic use states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import { useState } from "react";
+import Cards from "./Components/Cards";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Game of Thrones</h1>
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Cards.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Card from "./card";
+
+function Cards() {
+  const [cards, setCards] = useState([
+    { id: 0, name: "Dayaneris Targaryan", status: "", img: "/images/01.jpg" },
+    { id: 0, name: "Dayaneris Targaryan", status: "", img: "/images/01.jpg" },
+    { id: 1, name: "Tirion Lanister", status: "", img: "/images/02.jpg" },
+    { id: 1, name: "Tirion Lanister", status: "", img: "/images/02.jpg" },
+    { id: 2, name: "Jon Snow", status: "", img: "/images/03.jpg" },
+    { id: 2, name: "Jon Snow", status: "", img: "/images/03.jpg" },
+    { id: 3, name: "Joefrey Barateon", status: "", img: "/images/04.jpg" },
+    { id: 3, name: "Joefrey Barateon", status: "", img: "/images/04.jpg" },
+    { id: 4, name: "Cersei Lanister", status: "", img: "/images/05.jpg" },
+    { id: 4, name: "Cersei Lanister", status: "", img: "/images/05.jpg" },
+    { id: 5, name: "Eddart Stark", status: "", img: "/images/06.jpg" },
+    { id: 5, name: "Eddart Stark", status: "", img: "/images/06.jpg" },
+    { id: 6, name: "Tywin Lanister", status: "", img: "/images/07.jpg" },
+    { id: 6, name: "Tywin Lanister", status: "", img: "/images/07.jpg" },
+    { id: 7, name: "Sandor Clegane", status: "", img: "/images/08.jpg" },
+    { id: 7, name: "Sandor Clegane", status: "", img: "/images/08.jpg" },
+  ]);
+  return (
+    <div className="container">
+      {cards.map((card, index) => {
+        return <Card card={card} />;
+      })}
+    </div>
+  );
+}
+
+export default Cards;
